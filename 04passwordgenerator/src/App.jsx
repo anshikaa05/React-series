c0ff2377122104252769766296583a2269fd9f2c
anshikaa05 <66886152+anshikaa05@users.noreply.github.com>
2025-04-15 02:14:40 +0530

    started new project

---
+++ 04passwordgenerator/src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [length, setlength] = useState(8;
+  const [Number, setNumber] = useState(0)
+  const [Character, setCharacter] = useState(0)
+  const [password, setpassword] = useState(0)
+
+  return (
+    <>
+      <div className=''>Password Generator</h1>
+    </>
+  )
+}
+
+export default App
